3fb65f7de4eab6dd64735d8af792627bf1cae163
Jor1sh <13287508286@163.com>
2025-03-18 16:36:06 +0000

    Add analysis of NewModel
    I've done my part, you can see its result in pdf, it still needs some conclusion.

--- code.R
+++ code.R
@@ -18,6 +18,7 @@ data$Education <- as.factor(data$Education)
 data$Marital_Status <- as.factor(data$Marital_Status)
 data$Occupation <- as.factor(data$Occupation)
 data$Sex <- as.factor(data$Sex)
+data$Hours_PW <- as.numeric(data$Hours_PW)
 data$Nationality <- as.factor(data$Nationality)
 str(data)
 
@@ -31,23 +32,80 @@ coef_df <- as.data.frame(coef_table)
 significant_vars <- coef_df[coef_df$`Pr(>|z|)` < 0.05, ]
 significant_vars
 
-stepwise_model <- step(model, direction = "both", trace = 0)
+# Order education level
+edu_levels <- c(
+  "Preschool", "1st-4th", "5th-6th", "7th-8th", "9th", "10th", 
+  "11th", "12th", "HS-grad", "Some-college", "Assoc-acdm", 
+  "Assoc-voc", "Bachelors", "Masters", "Prof-school", "Doctorate"
+)
+data$Education <- factor(data$Education, levels = edu_levels, ordered = TRUE)
+data$Education <- as.numeric(data$Education)
+
+# Order nationality level
+data$Nationality <- as.factor(data$Nationality)
+data$Nationality <- as.numeric(data$Nationality)
+
+# Merge Occupation
+levels(data$Occupation) <- ifelse(levels(data$Occupation) %in% c("Exec-managerial"), 
+                                  levels(data$Occupation), "Other")
+data$Occupation <- factor(data$Occupation)
+data$Occupation <- relevel(data$Occupation, ref = "Other") # Set “Other” as the base group
+
+# Merge Marital Status
+levels(data$Marital_Status) <- ifelse(levels(data$Marital_Status) %in% c("Married-civ-spouse"), 
+                                      levels(data$Marital_Status), "Other")
+data$Marital_Status <- factor(data$Marital_Status)
+data$Marital_Status <- relevel(data$Marital_Status, ref = "Other") # Set “Other” as the base group
+
+model_new <- glm(Income ~ Age + Education + Marital_Status + Occupation + Sex + Hours_PW + Nationality, 
+                 data = data, 
+                 family = binomial)
+summary(model_new)
+
+stepwise_model <- step(model_new, direction = "both", trace = 0)
 summary(stepwise_model)
+
 stepwise_aic <- AIC(stepwise_model)
 print(paste("Stepwise AIC: ", stepwise_aic))
 
 num_data <- data[, sapply(data, is.numeric)]
 cor_matrix <- cor(num_data)
 print(cor_matrix)
-data_encoded <- model.matrix(~ Education + Marital_Status + Occupation + Sex + Nationality - 1, data = data)
+
+data_encoded <- model.matrix(~ Marital_Status + Occupation + Sex- 1, data = data)
 cor_matrix_encoded <- cor(data_encoded)
 print(cor_matrix_encoded)
 
-x <- cbind(data_encoded, data$Age)
+x <- model.matrix(Income ~ Age + Education + Marital_Status + Occupation + Sex + Hours_PW + Nationality - 1, data = data)
 y <- data$Income
-lasso_model <- glmnet(x, y, alpha = 1)
-cv_lasso <- cv.glmnet(x, y, alpha = 1)
-print(paste("Best lambda: ", cv_lasso$lambda.min))
+
+cv_lasso <- cv.glmnet(x, y, alpha = 1, family = "binomial")
+print(paste("Best lambda for Lasso: ", cv_lasso$lambda.min))
 plot(cv_lasso)
+
 final_lasso_model <- glmnet(x, y, alpha = 1, lambda = cv_lasso$lambda.min)
-print(coef(final_lasso_model))
+coef(final_lasso_model)
+
+# Extract coefficients from the model_new
+coefnew_df <- as.data.frame(summary(model_new)$coefficients)
+coefnew_df$Variable <- rownames(coefnew_df)
+coefnew_df <- coefnew_df[coefnew_df$`Pr(>|z|)` < 0.05, ]
+coefnew_df$Odds_Ratio <- exp(coefnew_df$Estimate)
+
+ggplot(coefnew_df, aes(x = reorder(Variable, Estimate), y = Estimate)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_errorbar(aes(ymin = Estimate - `Std. Error`, ymax = Estimate + `Std. Error`), width = 0.2) +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "Log-Odds of Significant Variables",
+       x = "Variables",
+       y = "Log-Odds (Coefficient)")
+
+ggplot(coefnew_df, aes(x = reorder(Variable, Odds_Ratio), y = Odds_Ratio)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_errorbar(aes(ymin = exp(Estimate - `Std. Error`), ymax = exp(Estimate + `Std. Error`)), width = 0.2) +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "Odds Ratios from model_new",
+       x = "Variables",
+       y = "Odds Ratio")
