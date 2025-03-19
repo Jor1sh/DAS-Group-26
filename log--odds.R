53b4cb25ec96c85cd2a0d8ea65787b6ba6757ada
Cassiiie-W <wangx040599@gmail.com>
2025-03-19 03:42:30 +0000

    Update log--odds.R

--- log--odds.R
+++ log--odds.R
@@ -1,18 +1,7 @@
-library(ggplot2)
-
-# Extract coefficients after step wise regression
-coef_df <- as.data.frame(summary(stepwise_model)$coefficients)
-coef_df$Variable <- rownames(coef_df)
-
+library(sjPlot)
 # Keep only significant variables
-coef_df <- coef_df[coef_df$`Pr(>|z|)` < 0.05, ]
-
-# （log-odds）
-ggplot(coef_df, aes(x = reorder(Variable, Estimate), y = Estimate)) +
-  geom_bar(stat = "identity", fill = "steelblue") +
-  geom_errorbar(aes(ymin = Estimate - `Std. Error`, ymax = Estimate + `Std. Error`), width = 0.2) +
-  coord_flip() +
-  theme_minimal() +
-  labs(title = "Log-Odds of Significant Variables",
-       x = "Variables",
-       y = "Log-Odds (Coefficient)")
+mod.coef.logodds<- stepwise_model %>%
+  summary() %>%
+  coef()
+plot_model(stepwise_model, show.values = TRUE, transform = NULL,
+           title = "Log-Odds (quality-good)", show.p = FALSE)
